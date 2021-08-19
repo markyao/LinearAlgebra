e83bcc7a6bd06b78e6c5e96c322ddd798997a08e
yaotailin <yaotailin@163.com>
2021-08-19 13:46:08 +0800

    矩阵转换图像应用

--- main_transformation.py
+++ main_transformation.py
@@ -1,3 +1,5 @@
+from playLA.Matrix import Matrix
+import math
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
@@ -11,4 +13,21 @@ if __name__ == '__main__':
     plt.xlim(-10, 10)
     plt.ylim(-10, 10)
     plt.plot(x, y)
+    # plt.show()
+
+    P = Matrix(points)
+
+    # T = Matrix([[2, 0], [0, 1.5]])
+    # T = Matrix([[1, 0], [0, -1]])
+    # T = Matrix([[-1, 0], [0, 1]])
+    # T = Matrix([[-1, 0], [0, -1]])
+    # T = Matrix([[1, 0.5], [0, 1]])
+    # T = Matrix([[1, 0], [0.5, 1]])
+
+    theta = math.pi / 3
+    T = Matrix([[math.cos(theta), math.sin(theta)], [-math.sin(theta), math.cos(theta)]])
+
+    P2 = T.dot(P.T())
+    plt.plot([P2.col_vector(i)[0] for i in range(P2.col_num())],
+             [P2.col_vector(i)[1] for i in range(P2.col_num())])
     plt.show()
